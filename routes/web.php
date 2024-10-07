5b2d8d436f4ef8f0d3c4aa1397939f1403f1b7a8
kevinElbert <kevinelbert84@gmail.com>
2024-10-07 22:03:42 +0700

    Add Code
    1. Add footer and header for register
    2. when run the server, automatically go to home

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+
+// Route::get('/home', function () {
+//     return view('home');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
