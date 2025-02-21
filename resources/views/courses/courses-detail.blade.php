bd8bb6ac0ccadd94da9455c3e5ace8453ce6d862
kevinElbert <kevinelbert84@gmail.com>
2025-02-21 22:38:52 +0700

    Revert "Update courses-detail.blade.php"
    This reverts commit 82b26638d5fd0782071539f1699a7f736aaa709f.

--- resources/views/courses/courses-detail.blade.php
+++ resources/views/courses/courses-detail.blade.php
@@ -152,15 +152,36 @@
     </div>
 
     <!-- Description (Kanan) -->
+    {{-- <div class="col-span-1 bg-white rounded-lg shadow-lg overflow-hidden">
+        <div class="bg-gray-50 px-6 py-4 border-b border-gray-200">
+            <h2 class="text-xl font-semibold text-gray-800">About This Course</h2>
+        </div>
+        <div class="p-6">
+            <h3 class="text-lg font-semibold text-gray-800 mb-3">What is {{ $currentSubTopic->title ?? $course->title }}?</h3>
+            <p id="description-text" class="text-gray-600 leading-relaxed">
+                {{ $currentSubTopic->description ?? $course->description }}
+            </p>
+        </div>
+    </div> --}}
     <div class="col-span-1 bg-white rounded-lg shadow-lg overflow-hidden">
         <div class="bg-gray-50 px-6 py-4 border-b border-gray-200">
             <h2 class="text-xl font-semibold text-gray-800 flex items-center">
                 <span>About This Course</span>
+                <button id="listen-about-course" class="ml-2 text-blue-500 hover:text-blue-700 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z" clip-rule="evenodd" />
+                    </svg>
+                </button>
             </h2>
         </div>
-        <div class="p-6 about-course-content">
-            <h3 class="text-lg font-semibold text-gray-800 mb-3">
-                What is {{ $currentSubTopic->title ?? $course->title }}?
+        <div class="p-6">
+            <h3 class="text-lg font-semibold text-gray-800 mb-3 flex items-center">
+                <span>What is {{ $currentSubTopic->title ?? $course->title }}?</span>
+                <button id="listen-what-is" class="ml-2 text-blue-500 hover:text-blue-700 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z" clip-rule="evenodd" />
+                    </svg>
+                </button>
             </h3>
             <p id="description-text" class="text-gray-600 leading-relaxed">
                 {{ $currentSubTopic->description ?? $course->description }}
